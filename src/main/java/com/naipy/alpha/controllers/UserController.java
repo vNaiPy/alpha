fdc4cbda8f3d5cc3c68a8764d0be42bde4c41a6a
Handrei.M.Souza <handrei.m.souza@hotmail.com>
2023-02-19 15:33:24 -0300

    Incremento do UserRepository e TestConfig com inserção de usuários teste

--- src/main/java/com/naipy/alpha/controllers/UserController.java
+++ src/main/java/com/naipy/alpha/controllers/UserController.java
@@ -1,11 +1,11 @@
 package com.naipy.alpha.controllers;
 
+import com.naipy.alpha.entities.User;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.naipy.alpha.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
---
+++ src/main/java/com/naipy/alpha/services/UserService.java
@@ -0,0 +1,7 @@
+package com.naipy.alpha.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
