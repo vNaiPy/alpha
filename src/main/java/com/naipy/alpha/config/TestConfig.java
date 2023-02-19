90b97a77947360790b102e778bfcbc9bcaa7602d
Handrei.M.Souza <handrei.m.souza@hotmail.com>
2023-02-19 18:13:35 -0300

    Inclusão do OrderStatus

--- src/main/java/com/naipy/alpha/config/TestConfig.java
+++ src/main/java/com/naipy/alpha/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.naipy.alpha.config;
 
 import com.naipy.alpha.entities.Order;
 import com.naipy.alpha.entities.User;
+import com.naipy.alpha.entities.enums.OrderStatus;
 import com.naipy.alpha.repositories.OrderRepository;
 import com.naipy.alpha.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 
         _userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.SHIPPED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         _orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
