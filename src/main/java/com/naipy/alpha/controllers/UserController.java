437086126ae15771ac9e302c27a5c33c0f07dd5e
Handrei.M.Souza <handrei.m.souza@hotmail.com>
2023-02-19 16:36:35 -0300

    Incremento das funções findAll e findById da entidade User

--- src/main/java/com/naipy/alpha/controllers/UserController.java
+++ src/main/java/com/naipy/alpha/controllers/UserController.java
@@ -2,19 +2,31 @@ package com.naipy.alpha.controllers;
 
 import com.naipy.alpha.entities.User;
 
+import com.naipy.alpha.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+    @Autowired
+    private UserService _userService;
 
     @GetMapping
-    public ResponseEntity<User> findAll () {
-        User user = new User(1L, "Carlos", "carlos@gmail.com", "11992091129", "123qwe");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll () {
+        List<User> userList = _userService.findAll();
+        return ResponseEntity.ok().body(userList);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById (@PathVariable Long id) {
+        User user = _userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
--- src/main/java/com/naipy/alpha/services/UserService.java
+++ src/main/java/com/naipy/alpha/services/UserService.java
@@ -1,7 +1,25 @@
 package com.naipy.alpha.services;
 
+import com.naipy.alpha.entities.User;
+import com.naipy.alpha.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository _userRepository;
+
+    public List<User> findAll () {
+        return _userRepository.findAll();
+    }
+
+    public User findById (Long id) {
+        Optional<User> userOptional = _userRepository.findById(id);
+        return userOptional.get();
+    }
+
 }
