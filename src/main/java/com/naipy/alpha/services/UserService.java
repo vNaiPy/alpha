c31e6cbb8c5c1dbbfbbb9919809ed283a6e13c6e
Handrei.M.Souza <handrei.m.souza@hotmail.com>
2023-02-27 22:22:03 -0300

    Exception handling - User findById

--- src/main/java/com/naipy/alpha/services/UserService.java
+++ src/main/java/com/naipy/alpha/services/UserService.java
@@ -2,6 +2,7 @@ package com.naipy.alpha.services;
 
 import com.naipy.alpha.entities.User;
 import com.naipy.alpha.repositories.UserRepository;
+import com.naipy.alpha.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById (Long id) {
         Optional<User> userOptional = _userRepository.findById(id);
-        return userOptional.get();
+        return userOptional.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert (User user) {
