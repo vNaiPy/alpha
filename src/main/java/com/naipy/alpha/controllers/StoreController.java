6cf4d2f2a76bcdae9d4056859dd68fdf9e0fc3f0
Handrei Morais <77680088+vNaiPy@users.noreply.github.com>
2023-05-16 21:43:38 -0300

    StoreController - Converting Rest to Graphql

--- src/main/java/com/naipy/alpha/controllers/StoreController.java
+++ src/main/java/com/naipy/alpha/controllers/StoreController.java
@@ -1,19 +1,14 @@
 package com.naipy.alpha.controllers;
 
-import com.naipy.alpha.entities.Product;
 import com.naipy.alpha.entities.Store;
-import com.naipy.alpha.entities.dto.ProductDTO;
 import com.naipy.alpha.services.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -23,19 +18,18 @@ public class StoreController {
     @Autowired
     private final StoreService _storeService;
 
-    @QueryMapping
-    public List<Store> findAll () {
-        return _storeService.findAll();
-    }
-
-    @GetMapping(value = "/mystore")
-    public ResponseEntity<Store> findStoreByCurrentUser () {
-        Store store = _storeService.findStoreByCurrentUser();
-        return ResponseEntity.ok().body(store);
-    }
-
     @MutationMapping
     public Store storeRegistration (@Argument Store store) {
         return _storeService.register(store);
+    }
+
+    @QueryMapping
+    public List<Store> findAllStore () {
+        return _storeService.findAll();
+    }
+
+    @QueryMapping
+    public Store findStoreByCurrentUser () {
+        return _storeService.findStoreByCurrentUser();
     }
 }
